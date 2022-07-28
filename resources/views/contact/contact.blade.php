412e2b620c317163c4121cfcf08fd98c885a4b16
ekpotosammuel <62830104+ekpotosammuel@users.noreply.github.com>
2022-07-28 17:14:33 +0100

    contct me form name

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -6,30 +6,30 @@
         <h1>CONTACT FORM</h1>
 
     </center>
-    <form action="https://formsubmit.co/ekpotosammuel1@gmail.com" method="POST">
+    <form action="https://formsubmit.co/a4c87fb5d0e512aa03b82890b01658ca" method="POST">
         <div class="row g-3">
             <div class="col-md-6">
                 <div class="form-floating">
-                    <input type="text" class="form-control border-0 bg-secondary" id="name" placeholder="Your Name">
+                    <input type="text" class="form-control border-0 bg-secondary" name="name" id="name" placeholder="Your Name">
                     <label for="name">Your Name</label>
                 </div>
             </div>
             <div class="col-md-6">
                 <div class="form-floating">
-                    <input type="email" class="form-control border-0 bg-secondary" id="email"
+                    <input type="email" class="form-control border-0 bg-secondary" name="email" id="email"
                         placeholder="Your Email">
                     <label for="email">Your Email</label>
                 </div>
             </div>
             <div class="col-12">
                 <div class="form-floating">
-                    <input type="text" class="form-control border-0 bg-secondary" id="subject" placeholder="Subject">
+                    <input type="text" class="form-control border-0 bg-secondary" name="subject" id="subject" placeholder="Subject">
                     <label for="subject">Subject</label>
                 </div>
             </div>
             <div class="col-12">
                 <div class="form-floating">
-                    <textarea class="form-control border-0 bg-secondary" placeholder="Leave a message here" id="message"
+                    <textarea class="form-control border-0 bg-secondary" placeholder="Leave a message here" name="comments, questions" id="message"
                         style="height: 200px"></textarea>
                     <label for="message">Message</label>
                 </div>
